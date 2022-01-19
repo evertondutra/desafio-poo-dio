8861263ce6349799d81c04f20c8ed6f1948719c1
Everton Dutra <evertondutra02@gmail.com>
2022-01-19 07:18:16 -0300

    fazendo abstração criando o objeto curso e mentoria

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria1 =new Mentoria();
+
+        mentoria1.setTitulo("Mentoria deJava");
+        mentoria1.setDescricao("Descrição mentoruia Java");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
